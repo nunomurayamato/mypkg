226a43f57db54b468479a4f9d1696b9307614892
Yamato Nunomura <s23c1103bp@s.chibakoudai.jp>
2024-12-05 12:37:23 +0900

    class

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -2,16 +2,27 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
+class Listener(Node):
+    def __init__(self):
+        super().__init__("listener")
+        self.sub = self.create_subscription(Int16, "countup", self.cb, 10)
 
-rclpy.init()
-node = Node("listener")
+#rclpy.init()
+#node = Node("listener")
 
 
-def cd(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+    def cb(self, msg):
+        self.get_logger().info(f"I heard: {msg.data}")
+    #global node
+    #node.get_logger().info("Listen: %d" % msg.data)
 
 
 def main():
-    pub = node.create_subscription(Int16, "countup", cd, 10)
+    rclpy.init()
+    node = Listener()
     rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+    #pub = node.create_subscription(Int16, "countup", cd, 10)
+    #rclpy.spin(node)
